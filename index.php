1b85d10566dfd6e3f6387581b6b30e3151703f97
Cyprian11 <barabaszcyprianti@gmail.com>
2022-11-17 11:10:19 +0100

    lekcja2

--- index.php
+++ index.php
@@ -1,25 +1,66 @@
 <!DOCTYPE html>
 <html>
     <head>
-    
+    <title>SKLEP</title>
+    <style>
+        .header{
+        background-color:powderblue;
+        }
+    </style>
     
     
     
     </head>
     <body>
-    
+    <div class="header">
     <?php
-    
+
         $con = new mysqli("127.0.0.1","root","","projekt_sklep");
-        
-        $res=$con->query($sql);
+        if(isset($_POST['login'])&&strlen($_POST['login'])){
+        }
+      
+            
+        $login="";
+        $password="";
+        $res=$con->query("SELECT * FROM users");
         $offers=$res->fetch_all(MYSQLI_ASSOC);
         $res->fetch_all();
-
-
-        
+        echo"<center><b><h1>ZALOGUJ SIĘ</h1></b></center>";
     ?>
-     
+     <br>
+
+    <center>
+    <form method="POST">
+        
+     Login:     <input type="text" name="login" placeholder="Podaj login"value="<?php echo $login;?>">
+     Password:  <input type="text" name="password" placeholder="Podaj hasło"value="<?php echo $password;?>">
+                <input type=submit value="ZALOGUJ">
     </form>
+    </center>
+    <?php
+    // print_r($_POST)
+    
+    // function logowanie($login, $haslo) {
+        
+    //     $mysqli->select_db("projekt_sklep",$con);
+    //     $wynik=mysql_query("SELECT * FROM users WHERE login='$login' AND password='$password'",$con);
+    //     if (mysql_num_rows($wynik)==1) {
+    //         mysql_close($con); return true;
+    //       } 
+    //         else {
+    //         mysql_close($con); return false;
+    //       }
+    //     }
+    // if (logowanie($login,$password)) {
+    //         echo "zalogowano";
+    //         }
+    //     else{
+    //         echo"nie zalogowano";
+    //         }
+    ?>
+  
+ 
+    
+    </div>
     </body>
 </html>
---
+++ wstawianie.sql
@@ -0,0 +1,8 @@
+--  USERS                  id   login  password  administrator 
+INSERT INTO users VALUES ('1', 'Ola', 'Ola123',  '0'); 
+-- ORDERS                  id   desc users_id   offers_id
+ INSERT INTO orders VALUES ('1', 'opis', '1',  '1'); 
+-- OFFERS                  id   user_id   descrition
+ INSERT INTO offers VALUES ('1', '1', 'opis'); 
+ 
+ 
